74f1960bc7ffee288ef324a46d980d52d5f2d003
ayoub <tofikch78@gmail.com>
2026-07-01 23:08:12 +0100

    PWA setup and app icon

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "amazzri-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/logo.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
